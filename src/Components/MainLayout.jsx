ffa0bd0f06ca38d68530484478b1448605066aa8
Christoph Kuri <chkuri@chkuri.de>
2024-08-07 16:47:44 +0200

    kleine Bugfixes

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -1,55 +1,79 @@
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Link,
-    Navigate,
-    Outlet,
-    Route,
-    RouterProvider,
-  } from 'react-router-dom';
-import React from 'react';
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Navigate,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import React from "react";
 import { useState, useEffect } from "react";
 
 const MainLayout = () => {
-    return (
-      <div className='flex flex-col min-h-screen'>
-        <header className='bg-gray-800 text-white p-4'>
-          <div className='container mx-auto flex justify-between items-center'>
-            <h1 className='text-2xl font-bold'>My Pokemon Game</h1>
-            <nav>
-              <ul className='flex space-x-4 font-semibold'>
-                <li>
-                  <Link to="/" className='hover:underline hover:underline-offset-4'>Home</Link>
-                </li>
-                <li>
-                  <Link to="/Details" className='hover:underline hover:underline-offset-4'>Details</Link>
-                </li>
-                <li>
-                  <Link to="/Roster">Roster</Link>
-                  </li>
-                  <li>
-                  <Link to="/Battle">Battle</Link>
-                  </li>
-                  <li>
-                  <Link to="/Leaderboard">Lederboard</Link>
-                  </li>
-                
-              </ul>
-            </nav>
-          </div>
-        </header>
-        
-        <div className="container mx-auto py-4">
-          <Outlet />
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="bg-gray-800 p-4 text-white">
+        <div className="container mx-auto flex items-center justify-between">
+          <h1 className="text-2xl font-bold">My Pokemon Game</h1>
+          <nav>
+            <ul className="flex space-x-4 font-semibold">
+              <li>
+                <Link
+                  to="/"
+                  className="hover:underline hover:underline-offset-4"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="Details/:id"
+                  className="hover:underline hover:underline-offset-4"
+                >
+                  Details
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Roster"
+                  className="hover:underline hover:underline-offset-4"
+                >
+                  Roster
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Battle"
+                  className="hover:underline hover:underline-offset-4"
+                >
+                  Battle
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/Leaderboard"
+                  className="hover:underline hover:underline-offset-4"
+                >
+                  Leaderboard
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
+      </header>
 
-        <footer className='bg-gray-800 text-white p-4'>
+      <div className="container mx-auto py-4">
+        <Outlet />
+      </div>
+
+      <footer className="bg-gray-800 p-4 text-white">
         <div className="container mx-auto text-center">
           &copy; {new Date().getFullYear()} My App. All rights reserved.
         </div>
-        </footer>
-      </div>
-    );
-  };
-  
-  export default MainLayout;
+      </footer>
+    </div>
+  );
+};
+
+export default MainLayout;
